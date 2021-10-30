e3ac4309193e1df172fa8efdf8d24c8229bc6044
Yasho <90166733+Yasho022@users.noreply.github.com>
2021-10-30 15:38:00 +0530

    Update Reddit_Scraper.py

--- Reddit_Scraper.py
+++ Reddit_Scraper.py
@@ -1,3 +1,4 @@
+from prawcore.exceptions import ResponseException
 from threading import Thread
 from itertools import cycle
 from time import time, sleep
@@ -6,14 +7,19 @@ import praw
 import json
 import os
 
+error = False
 done = False
 start = time()
 
 
 def animate(message):
     for c in cycle([f'⡿ {message}', f'⣟ {message}', f'⣯ {message}', f'⣷ {message}', f'⣾ {message}', f'⣽ {message}', f'⣻ {message}', f'⢿ {message}']):
+        if error:
+            print(f'\r')
+            break
         if done:
-            print(f'\u001B[32m\r✔️  Task Completed Successfully in \u001B[33m{str (round(time() - start, 2))}s \u001B[0m \n')
+            print(
+                f'\r\u001B[32m\r✔️  Podcast Successfully Generated \u001B[33m{str (round(time() - start, 2))}s \u001B[0m \r')
             break
         stdout.write('\r' + '\u001B[36m' + c)
         stdout.flush()
@@ -22,26 +28,33 @@ def animate(message):
 
 Thread(target=animate, args=("Scraping Reddit Data",)).start()
 
-reddit = praw.Reddit(client_id='client_id',
-                     client_secret='client_secret', user_agent='user_agent')
+try:
+    reddit = praw.Reddit(client_id='client_id',
+                         client_secret='client_secret', user_agent='user_agent')
 
-scraped_data = {
-    "title": [],
-    "body": [],
-    "author": []
-}
+    scraped_data = {
+        "title": [],
+        "body": [],
+        "author": []
+    }
 
-hot_posts = reddit.subreddit('stories').hot(limit=300)
-for post in hot_posts:
-    scraped_data["body"].append(str(post.selftext))
-    scraped_data["title"].append(str(post.title))
-    scraped_data["author"].append(str(post.author))
+    hot_posts = reddit.subreddit('stories').hot(limit=300)
+    for post in hot_posts:
+        scraped_data["body"].append(str(post.selftext))
+        scraped_data["title"].append(str(post.title))
+        scraped_data["author"].append(str(post.author))
+
+except Exception:
+    error = True
+    print("\r\u001B[31mError 401: Bad Credentials\u001B[0m")
+    exit()
 
 json_data = json.dumps(scraped_data, indent=4)
 
 try:
     os.mkdir('Stories')
-except FileExistsError: pass
+except FileExistsError:
+    pass
 finally:
     os.chdir('Stories')
 
